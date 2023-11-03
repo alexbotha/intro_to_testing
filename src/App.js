063a0f55c6a782f6d0eb19d8ba7f4419aef419af
alexbotha <alexbotha@hotmail.co.uk>
2023-11-03 15:58:19 +0000

    nested ternary and exporting function for tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import "./App.css";
 import { useState } from "react";
 
+export function replaceCamel(colorName) {
+  return colorName.split(/(?=[A-Z])/).join(" ");
+  // .split("")
+  // .map((char, index) => {
+  //   if (index === 0 || char !== char.toUpperCase()) {
+  //     return char;
+  //   } else {
+  //     return " " + char
+  //   }
+  // })
+  // .join("");
+}
+
 function App() {
   const [color, setColor] = useState(true);
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div>
       <button
         onClick={() => setColor(!color)}
-        style={{ backgroundColor: color ? "red" : "blue" }}
+        style={{
+          backgroundColor: !disabled ? (color ? "red" : "blue") : "grey",
+        }}
+        disabled={disabled}
       >
         Change to {color ? "blue" : "red"}
       </button>
+
+      <input
+        type="checkbox"
+        id="aaa"
+        label="aaa"
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      {/* We can search for a specific input via a label in our tests by giving our input an id and then using htmlFor in our label to link them */}
+      <label htmlFor="aaa">aaa</label>
     </div>
   );
 }
