e84b3eb728640f363e19a57e252cf4ca5a3f4a17
alexbotha <alexbotha@hotmail.co.uk>
2023-11-03 15:57:58 +0000

    describe and nested tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { replaceCamel } from "./App";
 
 import { logRoles } from "@testing-library/dom";
 
@@ -20,4 +21,54 @@ test("button has correct initial color, and updates when clicked", () => {
 
   // expect buttont text to change as well
   expect(colorButton).toHaveTextContent("Change to red");
+});
+
+test("initial conditions", () => {
+  render(<App />);
+
+  //check button starts out enabled
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+
+  //check that the check box starts out unchecked
+  const checkBox = screen.getByRole("checkbox", { name: "aaa" });
+  expect(checkBox).not.toBeChecked();
+});
+
+test("when check box is checked", () => {
+  render(<App />);
+
+  const checkBox = screen.getByRole("checkbox");
+  const button = screen.getByRole("button");
+
+  fireEvent.click(checkBox);
+  expect(button).toBeDisabled();
+  expect(button).toHaveStyle({ backgroundColor: "grey" });
+
+  fireEvent.click(checkBox);
+  expect(button).toBeEnabled;
+});
+
+test("checkbox disabled backgroundcolor = grey", () => {
+  render(<App />);
+
+  const checkBox = screen.getByRole("checkbox", { name: "aaa" });
+  const button = screen.getByRole("button");
+
+  fireEvent.click(checkBox);
+  expect(button).toHaveStyle({ backgroundColor: "grey" });
+});
+
+describe("spaces before camel cased letters", () => {
+  test("works for no inner capital letters", () => {
+    expect(replaceCamel("Red")).toBe("Red");
+  });
+
+  test("works for one inner cap letter", () => {
+    expect(replaceCamel("MidnightBlue")).toBe("Midnight Blue");
+  });
+
+  test("works for multiple cap letters", () => {
+    expect(replaceCamel("MediumVioletRed")).toBe("Medium Violet Red");
+  });
 });
